0438b5792f3e79fa0c98a777d63cad9c40f9b365
pPiyushK <kumarpiyush14024@gmail.com>
2026-09-13 14:33:02 +0530

    create table for medical profile

--- backend/database/LIVOX.sql
+++ backend/database/LIVOX.sql
@@ -10,4 +10,18 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE medical_profile (
+	name VARCHAR(50) PRIMARY KEY,
+    dob date NOT NULL,
+    bloodgroup VARCHAR(5) NOT NULL,
+    phone INT NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    Allergies VARCHAR(100),
+    Existing_conditions VARCHAR(100),
+    Current_medications VARCHAR(100),
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
     
